9b9185edac1623d6047188f0b1491ca8d7ec5bae
harcom102 <53519642+harcom102@users.noreply.github.com>
2023-11-02 11:31:29 +0700

    Add Enemy & Player Death States
    Added Enemy getting removed when Enemy HP reaches 0.
    
    Added Player getting a Game Over Screen when Player HP reaches 0.

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+    //Shows Game Over Screen
+    public void ShowGameOver()
+    {
+        gameObject.SetActive(true);
+    }
+}
